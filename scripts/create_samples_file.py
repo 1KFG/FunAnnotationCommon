70a1177f19fc57120c115735c7c39ba5176003b8
Jason Stajich <jasonstajich.phd@gmail.com>
2026-05-12 12:15:53 -0700

    update to differentiate the two speies cols

--- scripts/create_samples_file.py
+++ scripts/create_samples_file.py
@@ -63,7 +63,7 @@ accession_taxonomy = 'ncbi_accessions_taxonomy.csv'
 outsamples = 'samples.csv'
 
 accession_dict = {}
-fields = ['ASMID','SPECIES', 'STRAIN', 'BIOPROJECT', 'NCBI_TAXONID', 'BUSCO_LINEAGE']
+fields = ['ASMID','SPECIES_IN', 'STRAIN', 'BIOPROJECT', 'NCBI_TAXONID', 'BUSCO_LINEAGE']
 with open(accessions, 'r', newline='') as f:
     reader = csv.reader(f)
     header = next(reader)
